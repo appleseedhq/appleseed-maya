40a7647e2d127d3a1ffea476900590b5dc14cc55
Mango-3 <numen22@hotmail.com>
2019-01-05 11:06:43 +0200

    Expose SPPM render controls

--- src/appleseedmaya/renderglobalsnode.h
+++ src/appleseedmaya/renderglobalsnode.h
@@ -5,7 +5,7 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2016-2018 Esteban Tovagliari, The appleseedhq Organization
+// Copyright (c) 2016-2019 Esteban Tovagliari, The appleseedhq Organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -79,6 +79,7 @@ class RenderGlobalsNode
   private:
     static MObject      m_passes;
 
+    // Adaptive tile sampler.
     static MObject      m_adaptiveSampling;
     static MObject      m_minPixelSamples;
     static MObject      m_maxPixelSamples;
@@ -87,6 +88,7 @@ class RenderGlobalsNode
 
     static MObject      m_tileSize;
 
+    // Pixel filter.
     static MObject      m_pixelFilter;
     static MStringArray m_pixelFilterKeys;
     static MObject      m_pixelFilterSize;
@@ -95,9 +97,11 @@ class RenderGlobalsNode
 
     static MObject      m_lightingEngine;
 
+    // Shading overrides.
     static MObject      m_diagnosticShader;
     static MStringArray m_diagnosticShaderKeys;
 
+    // Path tracer.
     static MObject      m_limitBounces;
     static MObject      m_globalBounces;
     static MObject      m_specularBounces;
@@ -110,9 +114,30 @@ class RenderGlobalsNode
     static MObject      m_maxRayIntensity;
     static MObject      m_clampRoughness;
 
+    // Environment and image-based lighting.
     static MObject      m_envLightNode;
     static MObject      m_backgroundEmitsLight;
 
+    // Stochastic progressive photon mapping.
+    static MObject      m_sppm_photon_type;
+    static MObject      m_sppm_direct_lighting_mode;
+    static MObject      m_sppm_enable_caustics;
+    static MObject      m_sppm_enable_ibl;
+    static MObject      m_sppm_photon_tracing_enable_bounce_limit;
+    static MObject      m_sppm_photon_tracing_max_bounces;
+    static MObject      m_sppm_photon_tracing_rr_min_path_length;
+    static MObject      m_sppm_photon_tracing_light_photons;
+    static MObject      m_sppm_photon_tracing_environment_photons;
+    static MObject      m_sppm_radiance_estimation_enable_bounce_limit;
+    static MObject      m_sppm_radiance_estimation_max_bounces;
+    static MObject      m_sppm_radiance_estimation_rr_min_path_length;
+    static MObject      m_sppm_radiance_estimation_initial_radius;
+    static MObject      m_sppm_radiance_estimation_max_photons;
+    static MObject      m_sppm_radiance_estimation_alpha;
+    static MObject      m_sppm_max_ray_intensity_set;
+    static MObject      m_sppm_max_ray_intensity;
+
+    // Motion blur.
     static MObject      m_motionBlur;
     static MObject      m_mbCameraSamples;
     static MObject      m_mbTransformSamples;
@@ -120,11 +145,14 @@ class RenderGlobalsNode
     static MObject      m_shutterOpen;
     static MObject      m_shutterClose;
 
+    // System settings.
     static MObject      m_renderingThreads;
     static MObject      m_maxTextureCacheSize;
 
+    // Experimental.
     static MObject      m_useEmbree;
 
+    // Denoiser.
     static MObject      m_denoiserMode;
     static MStringArray m_denoiserModeKeys;
 
@@ -157,7 +185,7 @@ class RenderGlobalsNode
     static MObject      m_positionAOV;
     static MObject      m_uvAOV;
 
-    // Render Stamp.
+    // Render stamp.
     static MObject      m_renderStamp;
     static MObject      m_renderStampString;
 
